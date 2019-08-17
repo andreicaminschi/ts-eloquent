69fb29d4d1ebc47e753a65a4508be5e94a237505
Andrei Caminschi <andrei.caminschi@scriptics.ro>
2019-08-17 18:19:21 +0300

    stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import EloquentPlugin from "@/eloquent";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(EloquentPlugin);
+
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
